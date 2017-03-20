0a893c3cba43e4054310d74e589fcc0fa5f7dbff
YuliaShevchenko137 <yulia.shevchenko@outlook.com>
2017-03-20 19:01:23 +0200

    XMLParser test has finished

--- model/orm/messages-impl/src/test/java/ua/sumdu/java/lab2/instant_messenger/message_impl/tests/XMLParserTest.java
+++ model/orm/messages-impl/src/test/java/ua/sumdu/java/lab2/instant_messenger/message_impl/tests/XMLParserTest.java
@@ -1,20 +1,35 @@
 package ua.sumdu.java.lab2.instant_messenger.message_impl.tests;
 
+import com.tngtech.java.junit.dataprovider.DataProvider;
 import com.tngtech.java.junit.dataprovider.DataProviderRunner;
+import com.tngtech.java.junit.dataprovider.UseDataProvider;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import ua.sumdu.java.lab2.instant_messenger.entities.MessageMapImpl;
+import ua.sumdu.java.lab2.instant_messenger.parsers.XMLParser;
+
+import java.io.File;
+import java.net.UnknownHostException;
+
+import static org.junit.Assert.assertTrue;
 
 
 @RunWith(DataProviderRunner.class)
 public class XMLParserTest {
-    @Test
-    public void write() throws Exception {
 
+    @DataProvider
+    public static Object[][] data() throws UnknownHostException {
+        return JsonParserTest.data();
     }
 
     @Test
-    public void read() throws Exception {
-
+    @UseDataProvider("data")
+    public void writeAndReadFile(MessageMapImpl map) throws Exception {
+        XMLParser XML_Parser = XMLParser.getInstance();
+        File file = File.createTempFile("file","test");
+        XML_Parser.write(map, file);
+        MessageMapImpl newMap = (MessageMapImpl) XML_Parser.read(file);
+        assertTrue(map.equals(newMap));
+        file.deleteOnExit();
     }
-
 }
