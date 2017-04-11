d4270a1c566cc47f41f8907ca2394eb01d97f846
Darkie <darkstua@gmail.com>
2017-04-11 08:23:14 +0300

    Added configuration module

---
+++ model/config/config-impl/src/main/java/ua/sumdu/java/lab2/instant_messenger/processing/SettingsParserImpl.java
@@ -0,0 +1,34 @@
+package ua.sumdu.java.lab2.instant_messenger.processing;
+
+import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ua.sumdu.java.lab2.instant_messenger.api.Settings;
+import ua.sumdu.java.lab2.instant_messenger.api.SettingsParser;
+
+import java.util.Map;
+
+public class SettingsParserImpl implements SettingsParser {
+    public static final Logger LOG = LoggerFactory.getLogger(SettingsParserImpl.class);
+
+    public String SettingsToJson(Settings settings) {
+        Map<String, String> settingsMap = settings.getSettingsMap();
+
+        JSONObject jsonObject = new JSONObject();
+        for (String setting : settingsMap.keySet()) {
+            String value = settingsMap.get(setting);
+            jsonObject.put(setting, settingsMap.get(setting));
+        }
+        return jsonObject.toString();
+    }
+
+    public Settings JsonToSettings(String json) {
+        Settings settings = new SettingsImpl();
+        JSONObject jsonObject = new JSONObject(json);
+
+        for (String setting : jsonObject.keySet()) {
+            settings.putSetting(setting, (String)jsonObject.get(setting));
+        }
+        return settings;
+    }
+}
---
+++ model/config/config-impl/src/test/java/ua/sumdu/java/lab2/instant_messenger/config_impl/tests/SettingsTest.java
@@ -0,0 +1,37 @@
+package ua.sumdu.java.lab2.instant_messenger.config_impl.tests;
+
+import com.tngtech.java.junit.dataprovider.DataProvider;
+import com.tngtech.java.junit.dataprovider.DataProviderRunner;
+import com.tngtech.java.junit.dataprovider.UseDataProvider;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import ua.sumdu.java.lab2.instant_messenger.api.Settings;
+import ua.sumdu.java.lab2.instant_messenger.api.SettingsParser;
+import ua.sumdu.java.lab2.instant_messenger.processing.SettingsImpl;
+import ua.sumdu.java.lab2.instant_messenger.processing.SettingsParserImpl;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertTrue;
+
+@RunWith(DataProviderRunner.class)
+
+public class SettingsTest {
+
+    @DataProvider
+    public static Object[][] data() throws IOException {
+        Settings settings = new SettingsImpl();
+        settings.putSetting("setting1", "value1");
+        settings.putSetting("setting2", "value2");
+        return new Object[][]{{settings}};
+    }
+
+    @Test
+    @UseDataProvider("data")
+    public void writeAndReadJsonString(Settings settings) throws IOException {
+        SettingsParser parser = new SettingsParserImpl();
+        String json = parser.SettingsToJson(settings);
+        Settings parsed_settings = parser.JsonToSettings(json);
+        assertTrue(parsed_settings.equals(settings));
+    }
+}
